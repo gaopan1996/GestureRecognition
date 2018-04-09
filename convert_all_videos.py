cf66174eff27961194fabc4b719b39fb62d863e7
Gao Pan <gaopan1996@gmail.com>
2018-04-09 03:40:16 -0400

    Added keras method to convert all images to arrays

--- convert_all_videos.py
+++ convert_all_videos.py
@@ -2,16 +2,33 @@ import numpy as np
 import cv2
 import os
 import argparse
-
+import keras.preprocessing.image as kpi
+from PIL import Image
 def convert_all():
     classifier = "goodbye"
     array_of_video_frames = np.array([],int)
     for num in range(1,31):
         for frame in range(1,21):
             file = "videos/"+str(classifier)+str(num)+"/frame"+str(frame)+".jpg"
+            #print(file)
             img_array = cv2.imread(file,0)
             array_of_video_frames = np.append(array_of_video_frames,img_array)
             h,w = img_array.shape
             
     array_of_video_frames = np.reshape(array_of_video_frames, (30,20,640,360))
     return array_of_video_frames
+def convert_all_keras():
+    classifier = "goodbye"
+    array_of_video_frames = np.array([],int)
+    for num in range(1,31):
+        for frame in range(1,21):
+            file = "videos/"+str(classifier)+str(num)+"/frame"+str(frame)+".jpg"
+            pimg = Image.open(file)
+            img_array = kpi.img_to_array(pimg)
+            #print(img_array.shape)
+            array_of_video_frames = np.append(array_of_video_frames,img_array)
+            
+    array_of_video_frames = np.reshape(array_of_video_frames, (30,20,640,360))
+    return array_of_video_frames
+
+convert_all_keras()
