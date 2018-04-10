5a3daeaee43e185f77162cfc2a682360b3f8217e
Gao Pan <gaopan1996@gmail.com>
2018-04-09 20:40:39 -0400

    Added y array that is used to classify

--- convert_all_videos.py
+++ convert_all_videos.py
@@ -18,17 +18,24 @@ def convert_all():
     array_of_video_frames = np.reshape(array_of_video_frames, (30,20,640,360))
     return array_of_video_frames
 def convert_all_keras():
-    classifier = "goodbye"
+    classifiers = np.array(["goodbye","hey","yes","no"])
     array_of_video_frames = np.array([],int)
-    for num in range(1,31):
-        for frame in range(1,21):
-            file = "videos/"+str(classifier)+str(num)+"/frame"+str(frame)+".jpg"
-            pimg = Image.open(file)
-            img_array = kpi.img_to_array(pimg)
-            #print(img_array.shape)
-            array_of_video_frames = np.append(array_of_video_frames,img_array)
-            
-    array_of_video_frames = np.reshape(array_of_video_frames, (30,20,640,360,1))
-    return array_of_video_frames
-
-convert_all_keras()
+    array_of_classifiers = np.array([])
+    number_of_videos = 30
+    for classifier in classifiers:
+        for num in range(1,number_of_videos+1):
+            for frame in range(1,21):
+                file = "videos/"+classifier+str(num)+"/frame"+str(frame)+".jpg"
+                try:
+                    pimg = Image.open(file)
+                except IOError:
+                    break
+                img_array = kpi.img_to_array(pimg)
+                pimg.close()
+                #print(img_array.shape)
+                array_of_video_frames = np.append(array_of_video_frames,img_array)
+                array_of_classifiers = np.append(array_of_classifiers,classifier)
+#    array_of_classifiers = np.reshape(array_of_classifiers, (
+    array_of_video_frames = np.reshape(array_of_video_frames, (number_of_videos,20,640,360,1))
+    array_of_classifiers = np.reshape(array_of_classifiers, (number_of_videos, 20))
+    return array_of_video_frames, array_of_classifiers
